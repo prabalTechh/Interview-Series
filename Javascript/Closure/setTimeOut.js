64ef69e6651624bac38071dbee4e0a820361968e
Prabal Chanpuria <id.prabal@gmail.com>
2024-05-14 18:33:01 +0530

    Add files via upload

---
+++ Javascript/Closure/setTimeOut.js
@@ -0,0 +1,98 @@
+// here we are using closure using setTimeout and rendering the first variable in 5 seconds
+
+
+
+// function x(){
+//     var first = 10;
+//     setTimeout(function y(){
+//         console.log(first);
+//     }, 5000);
+
+// }
+// x();
+
+
+// Here is a problem ... You have to print 1-5 in each respective interval of time :-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function problem(){
+//     for(let i = 1 ; i<=5 ; i++){
+//         setTimeout(()=> console.log(i) , i * 1000);
+//     }
+// }
+// problem();
+
+
+// suppose interviewer asks for using var intead of let in for loop?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function problem(){
+//     for(var i = 1 ; i<6 ; i++){
+//         function newfun(x){
+//             setTimeout(()=>console.log(x), x * 1000);
+//         }
+//         newfun(i);
+
+//     }
+// }
+// problem();
+
+
+
